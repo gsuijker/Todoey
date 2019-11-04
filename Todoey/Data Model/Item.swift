b79b5966cb6364a62090f92d474d48bbb5573472
glenn suijker <glenn.suijker@gmail.com>
2019-11-04 20:02:06 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Iris Wagner on 03/11/2019.
+//  Copyright © 2019 Glenn Suijker. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
